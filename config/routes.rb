82e376993ca3b2375a5f8b1220256b0160b228fe
imaizumi <metallicaslayer2002@yahoo.co.jp>
2019-11-21 00:17:45 +0900

    認証機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  root 'home#index'
-  get 'comments/create'
-
-  get 'comments/destroy'
-
+  get 'mypage', to: 'users#me'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sesssions#delete' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'boards#index'
   # get 'boards', to: 'boards#index'
   # get 'boards/new', to: 'boards#new'
   # post '/boards', to: 'boards#create'
   # get 'boards/:id', to: 'boards#show'
+  root 'home#index'
+  resources :users, only: %i[new create]
   resources :boards
   resources :comments, only: %i[create destroy]
 end
